a74aa5b0298bd00a06212d84989d71c756e6c25b
fabricio <fabriciofraga15@gmail.com>
2018-10-22 22:10:35 -0300

    log

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ $(document).ready(() => {
   });
   const myMap = () => {
     var mapProp = {
-      center: new google.maps.LatLng(51.508742, -0.120850),
+      center: new google.maps.LatLng(-19,9388, -43,9989),
       zoom: 5,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
